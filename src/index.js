a12153a687441d486b97ef6b717eac11ce6d1f31
Ben Junya <ben@thebouqs.com>
2019-03-23 08:31:44 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -39,10 +39,45 @@ function getParamsFromURL(template, url) {
 
 }
 
+function getMinimum(array) {
+  if (array.length === 1) {
+    return array[0];
+  }
+  const low = array.slice(0, Math.floor(array.length / 2));
+  const high = array.slice(Math.floor(array.length / 2));
+
+  if (low[low.length - 1] > high[0]) {
+    return getMinimum(low);
+  } else {
+    return getMinimum(high);
+  }
+}
+
 
 
 module.exports = {
   diffObjects,
-  getParamsFromURL
+  getParamsFromURL,
+  getMinimum
 };
+
+[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  [ 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ]
+
+  [ 20, 21, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+    [ 20, 21, 1, 2, 3 ]
+
+    [ 4, 5, 6, 7, 8, 9 ]
+
+      [ 20, 21 ]
+
+      [ 1, 2, 3 ]
+
+
+
+
+
+
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,7 @@
 const  {
   diffObjects,
-  getParamsFromURL } = require('./index');
+  getParamsFromURL,
+  getMinimum } = require('./index');
 
 describe('diffObjects', () => {
 
@@ -9,5 +10,38 @@ describe('diffObjects', () => {
 
 describe('getParamsFromURL', () => {
 
-
 });
+
+describe('getMinimum', () => {
+
+  it('[5,6,7,1,2,3,4]', () => {
+    expect(getMinimum([5,6,7,1,2,3,4])).toBe(1);
+  });
+
+  it('[10,11,12,13,14,15,16,17,18,19,20,21,1,2,3,4,5,6,7,8,9]', () => {
+    expect(getMinimum(
+      [10,11,12,13,14,15,16,17,18,19,20,21,1,2,3,4,5,6,7,8,9])
+    ).toBe(1)
+  })
+});
+
+function findMinimum(arr) {
+  if (!arr || arr.length == 0) return null
+  if (arr.length == 1) return arr[0]
+  let lo = 0,
+      hi = arr.length - 1
+  const pivot = arr[arr.length - 1]
+
+  while (lo <= hi) {
+    const mid = lo + Math.floor(hi - lo)
+    if (arr[mid] <= pivot && (mid == 0 || arr[mid] < arr[mid-1])) {
+      return arr[mid]
+    } else if (arr[mid] > pivot) {
+      lo = mid + 1
+    } else {
+      hi = mid - 1
+    }
+  }
+
+  return null
+}
