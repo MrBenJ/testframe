e85bbc1e9942067a092e6c74e065e8922eb4de3d
Ben Junya <ben@thebouqs.com>
2019-03-28 13:51:55 -0700

    WIP for BST

--- src/bst.js
+++ src/bst.js
@@ -9,41 +9,77 @@ BinarySearchTree.prototype.insert = function (value) {
     ? 'left'
     : 'right';
 
-    if(this[subtree]) {
-      this[subtree].insert(value);
-    } else {
-      this[subtree] = new BinarySearchTree(value);
-    }
+  if (value === this.value) {
+    return `Value ${value} already exists in tree`;
+  }
+
+  if (this[subtree]) {
+    this[subtree].insert(value);
+  } else {
+    this[subtree] = new BinarySearchTree(value);
+  }
 }
 
 BinarySearchTree.prototype.get = function (value) {
-  if(this.value === value) {
+  if (this.value === value) {
     return this;
   }
 
   if (this.left === null & this.right === null) {
     return null;
   }
-  const subTree = value < this.value
+
+  const subtree = value < this.value
     ? 'left'
     : 'right';
 
-  this[subtree].get(value);
+  return this[subtree].get(value);
 
 }
 
-BinarySearchTree.prototype.max = function (value) {
+BinarySearchTree.prototype.max = function () {
+  if (this.right) {
+    return this.right.max();
+  }
+
+  return this.value;
 
 }
 
-BinarySearchTree.prototype.min = function (value) {
+BinarySearchTree.prototype.min = function () {
+  if (this.left) {
+    return this.left.min();
+  }
 
+  return this.value;
 }
 
 BinarySearchTree.prototype.contains = function (value) {
+  if (this.value === value) {
+    return true;
+  }
 
+  if (value < this.value) {
+    return Boolean(this.left) && this.left.contains(value);
+  } else {
+    return Boolean(this.right) && this.right.contains(value);
+  }
 }
 
+BinarySearchTree.prototype.delete = function (value) {
+  if (this.value === value) {
+    if (this.left === null && this.right === null) {
+      this.value = null;
+    }
 
+  }
+
+  const subtree = value < this.value
+    ? 'left'
+    : 'right';
+
+  this[subtree].delete(value);
+
+}
 
 module.exports = BinarySearchTree;
--- src/bst.test.js
+++ src/bst.test.js
@@ -23,23 +23,122 @@ describe('BinarySearchTree', () => {
 
       expect(bst.right.value).toBe(15);
     });
+
+    it('does not allow duplicates', () => {
+      const bst = new BinarySearchTree(5);
+
+      const resp = bst.insert(5);
+
+      expect(resp).toBe('Value 5 already exists in tree');
+    });
   });
 
   describe('get', () => {
-    it('Get a shallow value', () => {
+    it('Returns a leaf node', () => {
+      const bst = new BinarySearchTree(5);
+      bst.insert(1);
+      bst.insert(4);
+      bst.insert(6);
+      bst.insert(9);
 
+      const result = bst.get(4);
+      expect(result.value).toBe(4);
+      expect(result.left).toBe(null);
+      expect(result.right).toBe(null);
+      expect(result).toBeInstanceOf(BinarySearchTree);
+    });
+
+    it('Returns a node with children', () => {
+      const bst = new BinarySearchTree(50);
+      bst.insert(55);
+      bst.insert(53);
+      bst.insert(56);
+
+      const result = bst.get(55);
+      expect(result.value).toBe(55);
+      expect(result.left).toBeInstanceOf(BinarySearchTree);
+      expect(result.right).toBeInstanceOf(BinarySearchTree);
+      expect(result.left.value).toBe(53);
     });
   });
 
   describe('max', () => {
+    it('Returns the maximum size value in the tree', () => {
+      const bst = new BinarySearchTree(5);
+      bst.insert(10);
+      bst.insert(100);
+      bst.insert(150);
+      bst.insert(99);
+      bst.insert(201);
+      bst.insert(13);
+      bst.insert(3);
+      bst.insert(75);
 
+      const resp = bst.max();
+
+      expect(resp).toBe(201);
+    });
   });
 
   describe('min', () => {
+    it('Returns the minimum size value in the tree', () => {
+      const bst = new BinarySearchTree(5);
+      bst.insert(10);
+      bst.insert(-5);
+      bst.insert(-100);
+      bst.insert(-50);
+      bst.insert(100);
+      bst.insert(150);
+      bst.insert(99);
+      bst.insert(201);
+      bst.insert(13);
+      bst.insert(3);
+      bst.insert(75);
 
+      const resp = bst.min();
+
+      expect(resp).toBe(-100);
+    });
   });
 
   describe('contains', () => {
+    it('Returns true if the tree has the value', () => {
+      const bst = new BinarySearchTree(5);
+      bst.insert(10);
+      bst.insert(-5);
+      bst.insert(-100);
+      bst.insert(-50);
+      bst.insert(100);
+      bst.insert(150);
+
+      expect(bst.contains(-50)).toBe(true);
+    });
+
+    it('Returns false if the tree does not have the value', () => {
+      const bst = new BinarySearchTree(5);
+      bst.insert(10);
+      bst.insert(-5);
+      bst.insert(-100);
+      bst.insert(-50);
+      bst.insert(100);
+      bst.insert(150);
+
+      expect(bst.contains(-90)).toBe(false);
+    });
+  });
+
+  describe('delete', () => {
+    const bst = new BinarySearchTree(50);
+    bst.insert(10);
+    bst.insert(70);
+    bst.insert(55);
+    bst.insert(20);
+
+    bst.delete(20);
+
+    expect(bst.contains(20)).toBe(false);
 
   });
 });
+
+
